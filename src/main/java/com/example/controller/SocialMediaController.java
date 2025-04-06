5ca22ebbb272cb7ac9adf8f77e1329b5e2953ee3
Sebastien Adam <sebastien1adam@gmail.com>
2025-04-06 08:12:09 +0000

    implemented createAccountHolder

--- src/main/java/com/example/controller/SocialMediaController.java
+++ src/main/java/com/example/controller/SocialMediaController.java
@@ -1,5 +1,18 @@
 package com.example.controller;
 
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.RequestBody;
+
+
+import com.example.entity.Account;
+import com.example.entity.Message;
+import com.example.service.AccountService;
+import com.example.service.MessageService;
+import com.example.exception.AccountAlreadyExistsException;
+
 
 /**
  * TODO: You will need to write your own endpoints and handlers for your controller using Spring. The endpoints you will need can be
@@ -7,6 +20,40 @@ package com.example.controller;
  * where applicable as well as the @ResponseBody and @PathVariable annotations. You should
  * refer to prior mini-project labs and lecture materials for guidance on how a controller may be built.
  */
-public class SocialMediaController {
 
+ @RestController
+public class SocialMediaController {
+    private final MessageService messageService;
+    private final AccountService accountService;
+
+    public SocialMediaController(AccountService accountService, MessageService messageService){
+        this.accountService = accountService;
+        this.messageService = messageService;
+    }
+
+    @PostMapping("/register")
+    public ResponseEntity<?> createAccountHandler(@RequestBody Account newAccount){
+
+        try{
+            Account res = accountService.createAccount(newAccount);
+            return ResponseEntity.accepted()
+                   .body(newAccount);
+        } catch (AccountAlreadyExistsException e) {
+            return ResponseEntity.status(HttpStatus.CONFLICT)
+                   .body(e.getMessage());
+        } catch (Exception e) {
+            return ResponseEntity.status(HttpStatus.BAD_REQUEST)
+                   .body(e.getMessage());
+        }
+    }
+
+    @PostMapping("/login")
+    public ResponseEntity<Account> login(){
+        return null;
+    }
+
+    @PostMapping("/messages")
+    public ResponseEntity<Message> createMessage(){
+        return null;
+    }
 }
