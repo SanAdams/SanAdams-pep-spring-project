48f89759406c0bb06460267432e2379f9588184d
Sebastien Adam <sebastien1adam@gmail.com>
2025-04-07 07:33:32 +0000

    add MessageService

--- src/main/java/com/example/service/MessageService.java
+++ src/main/java/com/example/service/MessageService.java
@@ -1,4 +1,16 @@
 package com.example.service;
 
+import org.springframework.stereotype.Service;
+
+import com.example.entity.Message;
+import com.example.repository.MessageRepository;
+
+@Service
 public class MessageService {
+    private MessageRepository messageRepository;
+
+    public MessageService(MessageRepository messageRepository){
+        this.messageRepository = messageRepository;
+    }
+
 }
