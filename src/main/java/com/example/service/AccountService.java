b51033461cfe645f9545a7f5895dfe0ab9202ba1
Sebastien Adam <sebastien1adam@gmail.com>
2025-04-07 07:32:47 +0000

    add AccountService and createAccount

--- src/main/java/com/example/service/AccountService.java
+++ src/main/java/com/example/service/AccountService.java
@@ -1,4 +1,39 @@
 package com.example.service;
 
+import org.springframework.stereotype.Service;
+
+import com.example.entity.Account;
+import com.example.exception.AccountAlreadyExistsException;
+import com.example.exception.BlankUsernameException;
+import com.example.exception.PasswordLengthException;
+import com.example.repository.AccountRepository;
+
+@Service
 public class AccountService {
+    private AccountRepository accountRepository;
+
+    public AccountService(AccountRepository accountRepository){
+        this.accountRepository = accountRepository;
+    }
+
+    public Account createAccount(Account newAccount){
+        Account res;
+        if (accountRepository.existsByUsername(newAccount.getUsername())){
+            throw new AccountAlreadyExistsException("An account with username: "+ newAccount.getUsername() + " already exists");
+        } 
+        else if(newAccount.getPassword().length() < 4){
+            throw new PasswordLengthException("Password must be 4 characters or more");
+        }
+        else if(newAccount.getUsername().isBlank()){
+            throw new BlankUsernameException("Username cannot be blank");
+        }
+        else{
+            accountRepository.save(newAccount);
+            res = accountRepository.findByUsername(newAccount.getUsername());
+            return res;
+        }
+    }
+
+    
+
 }
