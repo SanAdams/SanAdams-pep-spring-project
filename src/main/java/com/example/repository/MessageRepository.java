2601bf4bf8aa5726bec6ad04b56a529dd9804e44
Sebastien Adam <sebastien1adam@gmail.com>
2025-04-08 18:36:50 +0000

    add accountExists query

--- src/main/java/com/example/repository/MessageRepository.java
+++ src/main/java/com/example/repository/MessageRepository.java
@@ -1,10 +1,15 @@
 package com.example.repository;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
 
 import com.example.entity.Message;
 
 @Repository
 public interface MessageRepository extends JpaRepository<Message, Integer>{
+    
+    @Query("SELECT COUNT(a) > 0 FROM Account a WHERE a.accountId = :accountId")
+    boolean accountExists(@Param("accountId") Integer accountId);
 }
