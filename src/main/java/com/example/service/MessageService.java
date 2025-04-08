8a503c181ba70e6092c1ee30a1fc8423cca71ff5
Sebastien Adam <sebastien1adam@gmail.com>
2025-04-08 18:33:07 +0000

    implement createMessage

--- src/main/java/com/example/service/MessageService.java
+++ src/main/java/com/example/service/MessageService.java
@@ -3,6 +3,9 @@ package com.example.service;
 import org.springframework.stereotype.Service;
 
 import com.example.entity.Message;
+import com.example.exception.account.AccountDoesNotExistException;
+import com.example.exception.message.MessageBlankException;
+import com.example.exception.message.MessageLengthException;
 import com.example.repository.MessageRepository;
 
 @Service
@@ -13,4 +16,21 @@ public class MessageService {
         this.messageRepository = messageRepository;
     }
 
+    public Message createMessage(Message newMessage){
+        meetsConstraints(newMessage);
+        return messageRepository.save(newMessage);
+    }
+
+    public boolean meetsConstraints(Message message){
+        if(message.getMessageText().isBlank()){
+            throw new MessageBlankException("Message cannot be blank");    
+        }
+        else if (message.getMessageText().length() > 255){
+            throw new MessageLengthException("Message cannot be over 255 characters");
+        } 
+        else if (!messageRepository.accountExists(message.getPostedBy())){
+            throw new AccountDoesNotExistException("The account " + message.getPostedBy() + " does not exist");
+        }        
+        return true;
+    }
 }
